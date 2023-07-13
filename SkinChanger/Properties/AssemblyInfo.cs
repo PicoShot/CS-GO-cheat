3665afcf3f15ddd099e55d717d92ad71226e8537
Pico Shot <116891491+PicoShot@users.noreply.github.com>
2023-07-14 00:57:39 +0300

    Add files via upload

---
+++ SkinChanger/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+
+[assembly: AssemblyTitle("SkinChanger")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("PicoShot ©")]
+[assembly: AssemblyProduct("SkinChanger")]
+[assembly: AssemblyCopyright("PicoShot©")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+
+[assembly: ComVisible(false)]
+
+
+[assembly: AssemblyVersion("1.3")]
+[assembly: AssemblyFileVersion("1.3")]
